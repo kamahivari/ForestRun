904ecc25c5cbef1d27b46736bdbacd20db883091
Onur Doğan <onur_nagod@outlook.com>
2023-12-10 14:18:37 +0300

    CevreSpawner.cs kaldırıldı
    sag-sol cevre icin ekstra bir script ile ayrılmıştı lakin zeminle senkron olmadığından ; zemin kombinasyonlarıyla birlikte tasarlanacak çevre prefabler

--- ForestRunner/Assets/Scripts/SpawnManager.cs
+++ ForestRunner/Assets/Scripts/SpawnManager.cs
@@ -26,6 +26,7 @@ public class SpawnManager : MonoBehaviour
     public void SpawnTriggerGiris()
     {
         zeminSpawner.MoveZemin();
-        cevreSpawner.SpawnPcevre();
+        cevreSpawner.SpawnPcevreSol();
+        cevreSpawner.SpawnPcevreSag();
     }
 }
