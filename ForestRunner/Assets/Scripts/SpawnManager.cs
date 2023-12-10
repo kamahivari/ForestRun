f19b42a7344c336442c22281073058fb48b828dd
Onur Doğan <onur_nagod@outlook.com>
2023-12-10 15:42:40 +0300

    SectionPreb olusturma
    Zemin ve Cevre Prefablerini modüler tasarlandı. Bu sayede farklı zemin ve çevre kombinasyonları yapılabilir, istediğin sırada listeden çekilir.
    
    Örnek olarak 4 Section Prefab yapıldı bu sayı zamanla artırılabilir.

--- ForestRunner/Assets/Scripts/SpawnManager.cs
+++ ForestRunner/Assets/Scripts/SpawnManager.cs
@@ -6,14 +6,14 @@ public class SpawnManager : MonoBehaviour
 {
 
     ZeminSpawner zeminSpawner;
-    CevreSpawner cevreSpawner; 
+    
 
 
     // Start is called before the first frame update
     void Start()
     {
         zeminSpawner = GetComponent<ZeminSpawner>();  //Spawn yönetimi sýnýfýndan nesneler çekilir.
-        cevreSpawner = GetComponent<CevreSpawner>();
+       
     }
 
     // Update is called once per frame
@@ -26,7 +26,6 @@ public class SpawnManager : MonoBehaviour
     public void SpawnTriggerGiris()
     {
         zeminSpawner.MoveZemin();
-        cevreSpawner.SpawnPcevreSol();
-        cevreSpawner.SpawnPcevreSag();
+       
     }
 }
