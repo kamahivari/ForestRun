75ae20694b16bbba6c7660ef414fbd4fd8ec0d70
Onur Doğan <onur_nagod@outlook.com>
2023-12-23 17:57:24 +0300

    Distance Hesaplama ve Kamera ayarları
    1- Distance hesaplamak için Canvas ve üzerine Text eklendi. UI objesi altında toplandı. Mesafe hesaplama ve Toplanabilir itemleri sayma işlemi GameManager.cs ile yönetilecek.
    
    2-Mesafe saymaya karakterin Z ekseni pozisyonundan başlıyor ; karakterimiz Z=-11,73 koordinatından başladığından ; Z=0 Pos çekildi buna göre kamera mesafesi tekrar düzenlendi.

---
+++ ForestRunner/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject player;
+
+    public Text uiMesafe; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int mesafe = Mathf.RoundToInt(player.transform.position.z); //player'ýn z ekseni üzerindeki hareketini RoundtoInt ile en yakýn integera yuvarlayarak katedilen mesafe girilir.
+        uiMesafe.text = mesafe.ToString() + " m"; 
+        }
+}
