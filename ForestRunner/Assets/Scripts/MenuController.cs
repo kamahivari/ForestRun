9359ed7f83b0c1a38c3ff5733ffdf53c9637f9e6
Hasan Arnavutoğlu <hasanversion2009@gmail.com>
2024-01-07 23:59:11 +0300

    MainMenuBestDiaVeScore

--- ForestRunner/Assets/Scripts/MenuController.cs
+++ ForestRunner/Assets/Scripts/MenuController.cs
@@ -2,15 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Text diamondText;
+    public Text scoreText;
     void Start()
     {
-        
+        int bestElmascore = PlayerPrefs.GetInt("bestElmas", 0);
+        int bestScore = PlayerPrefs.GetInt("bestScore", 0);
+        diamondText.text=bestElmascore.ToString() + "x";
+        scoreText.text=bestScore.ToString() + "m";
     }
-
+     
     // Update is called once per frame
     void Update()
     {
@@ -19,6 +25,7 @@ public class MenuController : MonoBehaviour
 
     public void StartGame()
     {
+        PlayerMove.isAlive = true;
         SceneManager.LoadScene("Level01");
     }
 
